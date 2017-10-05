f870f05fe113208d1d8f42f0dbc334099e2c5928
osboxes.org <otaviio@gmail.com>
2017-10-05 09:19:13 -0400

    Adjust runner to default path

--- src/julia-set/activity-2/run.sh
+++ src/julia-set/activity-2/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 SIMGRID_BIN=~/simgrid/SimGrid-3.13/bin
 
-$SIMGRID_BIN/smpirun -np $1 -hostfile ../../../hostfile/cluster_hostfile.txt \
+#$SIMGRID_BIN/smpirun -np $1 -hostfile ../../../hostfile/cluster_hostfile.txt \
+#-platform ../../../platform/crossbar.xml ./julia_set $2 \
+#--cfg=smpi/running-power:1 \
+#--cfg=smpi/privatize-global-variables:yes
+
+smpirun -np $1 -hostfile ../../../hostfile/cluster_hostfile.txt \
 -platform ../../../platform/crossbar.xml ./julia_set $2 \
 --cfg=smpi/running-power:1 \
 --cfg=smpi/privatize-global-variables:yes
